d34ad7a9f1f8d334a94250f769a600ee87c13583
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-01-29 16:20:35 -0500

    adjusted some styling, and added malolesbain experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), icon()]
+});
